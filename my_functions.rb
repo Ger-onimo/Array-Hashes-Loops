9aeb8fd3531968e8d77bfac8c2630a8aa3c2a8be
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-11-21 16:21:47 +0000

    Sum Array

--- my_functions.rb
+++ my_functions.rb
@@ -4,3 +4,26 @@ def add_array_lengths(array1, array2)
  return array1.length + array2.length
 
 end
+
+############
+
+def sum_array(numbers)
+
+  total = 0
+  for number in numbers
+  # number.each do |i|
+    total += number
+  end  
+end
+
+# # Write a function that has a parameter `numbers`
+# # It should return the sum value of an array passed as the argument
+#
+# def test_sum_array
+#   # arrange
+#   numbers = [ 1, 2, 3, 4, 5 ]
+#   # act
+#   result = sum_array( numbers )
+#   # assert
+#   assert_equal( 15, result )
+# end
