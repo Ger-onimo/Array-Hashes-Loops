d75b54e2604d6bc765f594cf3770adc10d9b932d
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-11-21 16:36:34 +0000

    sum array with for

--- my_functions.rb
+++ my_functions.rb
@@ -9,21 +9,13 @@ end
 
 def sum_array(numbers)
 
+  # sets total to 0
   total = 0
-  for number in numbers
-  # number.each do |i|
-    total += number
-  end  
-end
 
-# # Write a function that has a parameter `numbers`
-# # It should return the sum value of an array passed as the argument
-#
-# def test_sum_array
-#   # arrange
-#   numbers = [ 1, 2, 3, 4, 5 ]
-#   # act
-#   result = sum_array( numbers )
-#   # assert
-#   assert_equal( 15, result )
-# end
+  #
+  for number in numbers
+    total = total + number
+    p total
+  end
+  return total
+end
